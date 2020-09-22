606de1e53f6d129ffeda2effe4f491fefe36a1c6
jasonchilcott <jasonchilcottcode@gmail.com>
2020-09-22 11:24:07 -0400

    added campers#new, all deliverables finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :activities, only: [:index, :show]
-  resources :campers, only: [:index, :show, :new]
+  resources :campers
   resources :signups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
